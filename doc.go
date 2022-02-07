06decd790eeea9defdb58e3935e60104f89826e3
mdm-code <code.madamczyk@gmail.com>
2022-02-08 00:23:39 +0100

    [FEAT] Added package in-code documentation
    This is important to explain what the package
    is doing and give examples of how to use its
    API directly in the code.

---
+++ doc.go
@@ -0,0 +1,47 @@
+/*
+The XDG Base Directory Specification states where specification files should
+be searched. It does so by defining base directories relative to which files
+are located. The details of the specification are laid out here:
+https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+
+This package supports most Unix-based operating systems. It should work fine
+on MacOS.
+
+Usage
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/mdm-code/xdg"
+)
+
+func main() {
+	// XDG base directory paths.
+	dirs := []struct {
+		msg string
+		f   func() string
+	}{
+		{"Home data directory: ", xdg.DataHomeDir},
+		{"Config home directory: ", xdg.CacheHomeDir},
+		{"State home directory: ", xdg.StateHomeDir},
+		{"Data directories: ", xdg.DataDirs},
+		{"Config directories: ", xdg.ConfigDirs},
+		{"Cache home directory: ", xdg.CacheHomeDir},
+		{"Runtime home directory: ", xdg.RuntimeDir},
+	}
+	for _, d := range dirs {
+		fmt.Println(d.msg, d.f())
+	}
+
+	// Search for file in data XDG directories.
+	fpath := "/prog/file"
+	if f, ok := xdg.Find(xdg.Data, fpath); ok {
+		fmt.Println(f)
+	} else {
+		fmt.Printf("ERROR: couldn't find %s\n", fpath)
+	}
+}
+*/
+package xdg
